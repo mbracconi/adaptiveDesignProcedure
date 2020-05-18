d14e068493d1605220436c6e2a708bc7539211ea
mbracconi <mauro.bracconi@polimi.it>
2020-05-18 20:24:59 +0200

    Update installation files

---
+++ adaptiveDesignProcedure/__init__.py
@@ -0,0 +1,56 @@
+"""
+    /*----------------------------------------------------------------*\
+    |                                                                  |
+    |   Adaptive Refinement Procedure for Machine Leraning             |
+    |                                                                  |
+    |------------------------------------------------------------------|
+    |                                                                  |
+    |  Author: Mauro Bracconi                                          |
+    |           mauro.bracconi@polimi.it                               |                         
+    |           Politecnico di Milano                                  |
+    |           Dipartimento di Energia                                |
+    |           Laboratory of Catalysis and Catalytic Processes        |   
+    |                                                                  |
+    |------------------------------------------------------------------|
+    |                                                                  |
+    |   Copyright(C) 2019-2020 - M. Bracconi                           |
+    |                                                                  |
+    |------------------------------------------------------------------|
+    |                                                                  |
+    |   Reference:                                                     |
+    |       M. Bracconi and M. Maestri, "Training set design for       |
+    |       Machine Learning techniques applied to the approximation   |
+    |       of computationally intensive first-principles kinetic      |
+    |       models", Chemical Engineering Journal, 2020,               |
+    |       DOI: 10.1016/j.cej.2020.125469                             |
+    |                                                                  |
+    |------------------------------------------------------------------|
+    |                                                                  |
+    |   Description:                                                   |
+    |       Python class implementing the Adaptive Refinment           |
+    |       Procedure for Machine Learning using ExtraTress            |
+    |       and derivative-based addition of new points                |   
+    |                                                                  |
+    |   Version:                                                       |
+    |       * 1.0 (02/10/2020): adaptive refinement procedure          |
+    |       * 1.1 (04/08/2020): added MinMax scaling of tabulation     |
+    |                           variables                              |
+    |       * 1.2 (05/09/2020): added control on second derivative     |
+    |                           to improve description around          |
+    |                           stationary points                      |
+    |                                                                  |
+    \*----------------------------------------------------------------*/
+"""
+
+
+#__all__ = [] # TBD
+
+__author__ = "Mauro Bracconi"
+__copyright__ = "Copyright 2020, Mauro Bracconi"
+__license__ = "BSD"
+__version__ = "1.0.0"
+__mail__ = 'mauro.bracconi@polimi.it'
+__maintainer__ = __author__
+__status__ = "Alpha"
+
+from .adaptiveDesignProcedure import adaptiveDesignProcedure
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+description = (
+    "A design procedure of the training data for Machine Learning"
+    "algorithms able to iteratively add datapoints according to"
+    "function discrete gradient"
+    "Reference:"
+	"Most of the theoretical aspects behind adaptiveDesignProcedure"
+	"are reported in:"
+	"M. Bracconi and M. Maestri, Training set design for "
+	"Machine Learning techniques applied to the approximation "
+	"of computationally intensive first-principles kinetic models,"
+	"Chemical Engineering Journal, 2020, "
+	"DOI: 10.1016/j.cej.2020.125469"
+    "\n"
+)
+
+setup(name='adaptiveDesignProcedure',
+	  version='0.0.1',
+	  description='adaptiveDesignProcedure',
+	  long_description=description,
+	  classifiers=[
+	  	'Development Status :: 5 - Production/Stable',
+	  	'License :: OSI Approved :: BSD License',
+	  	'Programming Language :: Python :: 3.6',
+	  	'Intended Audience :: Science/Research',
+	  	'Topic :: Scientific/Engineering :: Chemical Engineering'
+	  ],
+	  keywords='machine learning; computational fluid dyamics',
+	  url='https://github.com/mbracconi/adaptiveDesignProcedure',
+	  author='Mauro Bracconi',
+	  author_email='mauro.bracconi@polimi.it',
+	  license='BSD',
+	  packages=['adaptiveDesignProcedure'],
+	  install_requires=[
+	  		'numpy',
+	  		'scipy',
+	  		'matplotlib',
+	  		'scikit-learn',
+	  		'Sphinx==1.4',
+	  		'sphinx_rtd_theme'
+	  ],
+	  test_suite='',
+	  tests_require=[''],
+	  include_package_data=True,
+	  zip_safe=False)
