b67e49ae9450a7017b58feecfb3af8a6f3a2cae2
mbracconi <mauro.bracconi@polimi.it>
2020-05-19 12:16:58 +0200

    Corrected tabulation

--- adaptiveDesignProcedure/adaptiveDesignProcedure.py
+++ adaptiveDesignProcedure/adaptiveDesignProcedure.py
@@ -87,7 +87,8 @@ class adaptiveDesignProcedure:
                                   
                                     { 'name' : 'B', 'min' : 1e-3, 'max' : 1, 'num' : 3, 'typevar' : 'log'}
                                   
-                                )           
+                                )  
+                                         
             out_variables : tuple[dictionary]
                 Dictionaries for each tabulation species reporting: 'name' -> variable name, 'typevar' -> type of variable (log,lin)
                 
