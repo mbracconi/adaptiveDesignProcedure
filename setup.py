e22d0cdcec549c3b3e850ef7a6575617e37ba6a2
Nestor F. Aguirre <nfaguirrec@gmail.com>
2022-10-21 13:02:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ description = (
 )
 
 setup(name='adaptiveDesignProcedure',
-	  version='1.3.0',
+	  version='1.4.0',
 	  description='adaptiveDesignProcedure',
 	  long_description=description,
 	  classifiers=[
