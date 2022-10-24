c2e7107bbb687a5db6f08e19455e827bd11a1b6c
Nestor Aguirre <aguirre@scm.com>
2022-10-24 07:23:54 +0200

    Tests reorganized -- SO203

---
+++ examples/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function runDir()
+{
+    example_name=$1
+
+    echo -n "Running $example_name ... "
+    python3 ${example_name}.py > ${example_name}.out 2> /dev/null
+    grep -A40 "> Training data" ${example_name}_orig.out > report.old
+    grep -A40 "> Training data" ${example_name}.out > report.new
+    check=`paste report.old report.new | awk '($0!~/^>/ && $0!~/^\s+$/){diff=sqrt(($4-$2)**2); if(diff>0.0){loc=1; print 1; exit} }END{if(loc==0) print 0}'`
+    if [ "$check" -eq 0 ]
+    then
+        echo "PASSED"
+        rm report.old report.new
+        rm ${example_name}.out
+        [ -x "clean.sh" ] && ./clean.sh
+    else
+        echo "FAILED"
+        paste -d "~" report.old report.new | sed 's/~/         |/g'
+        exit -1
+    fi
+}
+
+function main()
+{
+    for dir in `ls`
+    do
+        [ $dir = "test.sh" ] && continue
+
+        cd $dir
+        runDir $dir
+        cd ..
+    done
+}
+
+main $*
