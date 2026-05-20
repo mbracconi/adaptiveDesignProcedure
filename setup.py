4007cc00217e23dc02af131492bc54daf4131497
Nestor Aguirre <nfaguirrec@gmail.com>
2026-05-20 10:59:49 +0200

    Upgrading Python Version => 3.8, 3.9, 3.10, 3.11, 3.12

--- setup.py
+++ setup.py
@@ -1,46 +1,53 @@
 from setuptools import setup
 
 description = (
-    "A design procedure of the training data for Machine Learning"
-    "algorithms able to iteratively add datapoints according to"
-    "function discrete gradient"
-    "Reference:"
-	"Most of the theoretical aspects behind adaptiveDesignProcedure"
-	"are reported in:"
-	"M. Bracconi and M. Maestri, Training set design for "
-	"Machine Learning techniques applied to the approximation "
-	"of computationally intensive first-principles kinetic models,"
-	"Chemical Engineering Journal, 2020, "
-	"DOI: 10.1016/j.cej.2020.125469"
+    "A design procedure of the training data for Machine Learning "
+    "algorithms able to iteratively add datapoints according to "
+    "function discrete gradient. "
+    "Reference: "
+    "Most of the theoretical aspects behind adaptiveDesignProcedure "
+    "are reported in: "
+    "M. Bracconi and M. Maestri, Training set design for "
+    "Machine Learning techniques applied to the approximation "
+    "of computationally intensive first-principles kinetic models, "
+    "Chemical Engineering Journal, 2020, "
+    "DOI: 10.1016/j.cej.2020.125469"
     "\n"
 )
 
-setup(name='adaptiveDesignProcedure',
-	  version='1.4.1',
-	  description='adaptiveDesignProcedure',
-	  long_description=description,
-	  classifiers=[
-	  	'Development Status :: 5 - Production/Stable',
-	  	'License :: OSI Approved :: BSD License',
-	  	'Programming Language :: Python :: 3.6',
-	  	'Intended Audience :: Science/Research',
-	  	'Topic :: Scientific/Engineering :: Chemical Engineering'
-	  ],
-	  keywords='machine learning; computational fluid dyamics',
-	  url='https://github.com/mbracconi/adaptiveDesignProcedure',
-	  author='Mauro Bracconi',
-	  author_email='mauro.bracconi@polimi.it',
-	  license='BSD',
-	  packages=['adaptiveDesignProcedure'],
-	  install_requires=[
-			'joblib',
-			'numpy<1.24',
-			'scikit-learn',
-			'boruta',
-			'packaging'
-	  ],
-	  extras_require={ 'docs': ['Sphinx', 'sphinx_rtd_theme'] },
-	  test_suite='',
-	  tests_require=[''],
-	  include_package_data=True,
-	  zip_safe=False)
+setup(
+    name='adaptiveDesignProcedure',
+    version='1.4.1',
+    description='adaptiveDesignProcedure',
+    long_description=description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemical Engineering'
+    ],
+    keywords='machine learning; computational fluid dyamics',
+    url='https://github.com/mbracconi/adaptiveDesignProcedure',
+    author='Mauro Bracconi',
+    author_email='mauro.bracconi@polimi.it',
+    license='BSD',
+    packages=['adaptiveDesignProcedure'],
+    python_requires='>=3.8, <3.13',
+    install_requires=[
+        'joblib',
+        'numpy',
+        'scikit-learn',
+        'boruta',
+        'packaging'
+    ],
+    extras_require={'docs': ['Sphinx', 'sphinx_rtd_theme']},
+    test_suite='',
+    tests_require=[''],
+    include_package_data=True,
+    zip_safe=False
+)
--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+[ -z "$PYTHON_EXEC" ] && export PYTHON_EXEC=python3
+
 function runDir()
 {
     example_name=$1
 
     echo -n "Running $example_name ... "
-    python3 ${example_name}.py > ${example_name}.out 2> ${example_name}.err
+    $PYTHON_EXEC ${example_name}.py > ${example_name}.out 2> ${example_name}.err
     grep -A40 "> Training data" ${example_name}_orig.out > report.old
     grep -A40 "> Training data" ${example_name}.out > report.new
     check=`paste report.old report.new | awk '($0!~/^>/ && $0!~/^\s+$/){diff=sqrt(($NF-$(NF/2))**2); if(diff>0.0){loc=1; print 1; exit} }END{if(loc==0) print 0}'`
@@ -26,10 +28,10 @@ function runDir()
 
 function main()
 {
-    echo "Using `python3 --version`"
-    echo "Using Scikit-learn `python3 -c "import sklearn; print(sklearn.__version__)"`"
+    echo "Using `$PYTHON_EXEC --version`"
+    echo "Using Scikit-learn `$PYTHON_EXEC -c "import sklearn; print(sklearn.__version__)"`"
     echo -n "Checking adaptiveDesignProcedure ... "
-    python3 -c "import adaptiveDesignProcedure" && echo "OK"
+    $PYTHON_EXEC -c "import adaptiveDesignProcedure" && echo "OK"
 
     for dir in `ls`
     do
